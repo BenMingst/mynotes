a149a6a4d301ba797176d694a6e6202faad105db
BenMingst <124937048+BenMingst@users.noreply.github.com>
2023-05-21 10:52:25 -0400

    update

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
-
 import 'package:mynotes/constants/routes.dart';
 
 class LoginView extends StatefulWidget {
@@ -72,9 +70,9 @@ class _LoginViewState extends State<LoginView> {
                   );
                 } else {
                   Navigator.of(context).pushNamedAndRemoveUntil(
-                  verifyEmailRoute,
-                  (route) => false,
-                );
+                    verifyEmailRoute,
+                    (route) => false,
+                  );
                 }
                 // ignore: use_build_context_synchronously
               } on FirebaseAuthException catch (e) {
